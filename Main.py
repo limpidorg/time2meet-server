ef2753990c64280f46a69ef58e34b48fc620d45b
Lincoln Yan <yyjlincoln@gmail.com>
2021-12-05 20:26:37 +1100

    Update: Added Helloworld

--- Main.py
+++ Main.py
@@ -8,4 +8,22 @@ FlaskProtocol = HTTPViaFlask()
 API.useProtocol(FlaskProtocol)
 API.useResponseHandler(JSONStandardizer())
 
+
+@API.endpoint("webroot-post", {
+    'httpmethods': ['POST'],
+    'httproute': '/',
+})
+def webroot(makeResponse):
+    return makeResponse(0, message="Helloworld from POST")
+
+@API.endpoint("webroot-delete", {
+    'httpmethods': ['DELETE'],
+    'httproute': '/',
+})
+def webroot(makeResponse):
+    return makeResponse(0, message="Helloworld from DELETE (do different stuff)")
+
+
 app = FlaskProtocol.app
+
+app.run(port=8080)
