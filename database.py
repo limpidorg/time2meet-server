3f608fc6e014dc101457d0698ce562029fbc9f50
Lincoln Yan <yyjlincoln@gmail.com>
2021-12-06 21:41:05 +1100

    Update: Updated database

--- database.py
+++ database.py
@@ -1,33 +1,36 @@
+from typing import List
 from mongoengine import Document, StringField
-from mongoengine.fields import EmailField, FloatField, ListField
+from mongoengine.document import EmbeddedDocument
+from mongoengine.fields import EmailField, EmbeddedDocumentListField, FloatField, ListField
 
+
+class PlannerPermission(Document):
+    userId = StringField(required=True, unique=True)
+    plannerId = StringField(required=True)
+    permissions = ListField(StringField(), default=[], required=True)
 
 class Planner(Document):
     plannerId = StringField(required=True, unique=True)
     plannerName = StringField(required=True)
     notBefore = FloatField(required=True)
     notAfter = FloatField(required=True)
+    # Default permission for all users. For an individual user, use PlannerPermission
+    permissions = ListField(StringField(), default=[], required=True)
 
-# Not using a global user account as that will complicate things. That's a feature for the future.
-
-# class User(Document):
-#     userId = StringField(required=True, unique=True)
-#     userName = StringField(required=True)
-#     email = EmailField()
-#     password = StringField()
-#     salt = StringField()
-#     # The user's time shift relative to GMT
-#     timeShift = FloatField(required=True)
-
-
-class PlannerUser(Document):
-    userId = StringField(required=True)
-    plannerId = StringField(required=True)
+class Token(EmbeddedDocument):
+    token = StringField(required=True)
+    expires = FloatField(required=True)
+    scopes = ListField(StringField(), default=[], required=True)
+    
+class User(Document):
+    userId = StringField(required=True, unique=True)
+    userName = StringField(required=True)
+    email = EmailField()
     password = StringField()
     salt = StringField()
     # The user's time shift relative to GMT
     timeShift = FloatField(required=True)
-
+    tokens = EmbeddedDocumentListField(Token)
 
 class TimePreference(Document):
     userId = StringField(required=True)
